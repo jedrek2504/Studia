411a17674b24080b962dabfd868e7bdabc8ad98f
Jedrzej Kostyk <242430@edu.p.lodz.pl>
2022-05-09 19:22:21 +0200

    .gitignore is now working

--- KOLOSY/ZROBIONE/StatekMKar2020/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jedre/OneDrive - Politechnika Łódzka/Pulpit/MKAR_2020_Statek/StatekMKar2020
-# Build directory: C:/Users/jedre/OneDrive - Politechnika Łódzka/Pulpit/MKAR_2020_Statek/StatekMKar2020/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test "C:/Users/jedre/OneDrive - Politechnika Łódzka/Pulpit/MKAR_2020_Statek/StatekMKar2020/out/build/x64-Debug/TestProject.exe" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(Test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jedre/OneDrive - Politechnika Łódzka/Pulpit/MKAR_2020_Statek/StatekMKar2020/CMakeLists.txt;64;add_test;C:/Users/jedre/OneDrive - Politechnika Łódzka/Pulpit/MKAR_2020_Statek/StatekMKar2020/CMakeLists.txt;0;")
--- Workshop/CarRental/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental
-# Build directory: /home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
