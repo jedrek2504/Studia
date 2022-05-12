e523b477c15470c9d260920e3a1f445d0be4a32d
Jedrzej Kostyk <242430@edu.p.lodz.pl>
2022-05-12 19:34:36 +0200

    .gitignore is now working

--- KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Studia/KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL
-# Build directory: /home/student/Studia/KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test "/home/student/Studia/KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL/cmake-build-debug/TestProject" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(Test PROPERTIES  _BACKTRACE_TRIPLES "/home/student/Studia/KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL/CMakeLists.txt;48;add_test;/home/student/Studia/KOLOSY/MOJE ZROBIONE/kwapisz_REGAL/KWAPISZ_REGAL/CMakeLists.txt;0;")
--- Workshop/CarRental/cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental/library
-# Build directory: /home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LibraryTest1 "LibraryTest" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(LibraryTest1 PROPERTIES  _BACKTRACE_TRIPLES "/home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental/library/CMakeLists.txt;41;add_test;/home/student/gitrepo/mkac_wt_1400_02/kostyk/Workshop/CarRental/library/CMakeLists.txt;0;")
