1be51b9468a4b0669b7659a953e6e645d3d3bd5b
Jedrzej Kostyk <242430@edu.p.lodz.pl>
2022-05-17 19:49:13 +0200

    fixed kacpro_start kolos 2022

---
+++ KOLOSY/starty_same/KOLOKWIUM_2022_KACPROWICZ/start_mojKolos/test/BoatRentalTest.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+using namespace std;
+namespace btt = boost::test_tools;
+
+BOOST_AUTO_TEST_SUITE(BoatRentalTest)
+
+    BOOST_AUTO_TEST_CASE(RentBoatTest) {
+
+    }
+    BOOST_AUTO_TEST_CASE(ReturnBoatTest) {
+
+
+    }
+BOOST_AUTO_TEST_SUITE_END()
