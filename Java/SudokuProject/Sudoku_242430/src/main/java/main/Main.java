b57df086e89b019f7a67dba68b7727fd070960cc
242430 Kostyk Jędrzej <242430@edu.p.lodz.pl>
2022-10-21 14:47:01 +0200

    zad2 - start

---
+++ Java/SudokuProject/Sudoku_242430/src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import model.SudokuBoard;
+
+
+public class Main {
+    public static void main(String[] args) {
+        var board = new SudokuBoard();
+
+        board.fillBoard();
+        System.out.println(board.toString());
+
+    }
+}
