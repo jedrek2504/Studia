53550cf7861c39edf5f7d2aa6bfeade1200702a8
242430 Kostyk Jędrzej <242430@edu.p.lodz.pl>
2023-01-13 18:55:46 +0100

    XML zad5 finished

--- Web/zad5/form.js
+++ Web/zad5/form.js
@@ -3,13 +3,13 @@ const loadButton = document.getElementById('loadButton');
 let fileContent = null;
 const saveButton = document.getElementById("saveButton");
 const addButton = document.querySelector('[name="addButton"]');
-let seria = document.getElementById('seria');
-let segment = document.getElementById('segment');
 let model = document.getElementById('model');
-let pojemnosc = document.getElementById('pojemnosc');
-let uklad = document.getElementById('uklad');
+let segment = document.getElementById('segment');
+let moc = document.getElementById('moc');
 let spalanie = document.getElementById('spalanie');
 let data = document.getElementById('data');
+let cena = document.getElementById('cena');
+let seria = document.getElementById('seria');
 const deleteCarButton = document.getElementById('deleteCarButton');
 const modifyCarButton = document.getElementById('modifyCarButton');
 const warning1 = document.getElementById('warning1');
@@ -20,10 +20,14 @@ const warning5 = document.getElementById('warning5');
 const warning6 = document.getElementById('warning6');
 const warning7 = document.getElementById('warning7');
 const warning8 = document.getElementById('warning8');
+const warning9 = document.getElementById('warning9');
+const warning10 = document.getElementById('warning9');
 var isLoaded = false;
 var isCarValid = false;
 var isModifyCarValid = false;
+const pozycja = 3;
 
+//git
 function loadFile() {
     const [file] = fileInput.files;
 
@@ -39,6 +43,7 @@ function loadFile() {
     isLoaded = true;
 }
 
+//git
 function handleSaveButton() {
     if (fileContent === null) {
         return alert("Nie wczytano pliku XML.");
@@ -54,66 +59,75 @@ function handleSaveButton() {
     link.click();
 }
 
+//wstepnie git ale raczje trzeba dopisac
+function removeOptions(){
+    const select1 = document.getElementById('deleteCar');
+    const select2 = document.getElementById('pickCar');
+
+    var selects = [select1,select2];
+    for(var i=0; i<2;i++){
+        while(selects[i].firstChild){
+            selects[i].removeChild(selects[i].firstChild);
+        }
+    }
+
+}
+
+//wstepnie git ale raczje trzeba dopisac
 function addDeleteOptions() {
     removeOptions();
 
-    // Add options to the "seria" dropdown menu
-    const seriaSelect = document.getElementById('seria');
-    const serie = fileContent.querySelector('serie');
-    for (const seria of serie.children) {
-        const option = document.createElement("option");
-        option.text = seria.getAttribute('seriaID');
-        seriaSelect.add(option);
+    const select = document.getElementById('deleteCar');
+    var cars = fileContent.querySelector('samochody');
+    var i = 1;
+    for(const car of cars.children){
+        var option = document.createElement("option");
+        option.setAttribute('value',i);
+        i++;
+        option.text = car.children[0].textContent;
+        select.add(option);
     }
 
-    // Add options to the "delete car" dropdown menu
-    const deleteCarSelect = document.getElementById('deleteCar');
-    const cars = fileContent.querySelector('samochody');
-    for (const car of cars.children) {
-        const option = document.createElement("option");
-        option.text = car.getAttribute('seriaID');
-        deleteCarSelect.add(option);
-    }
-
-    // Add options to the "modify car" dropdown menu
-    const modifyCarSelect = document.getElementById('modifyCar');
-    for (const car of cars.children) {
-        const option = document.createElement("option");
-        option.text = car.getAttribute('seriaID');
-        modifyCarSelect.add(option);
+    const select1 = document.getElementById('pickCar');
+    i = 1;
+    for(const car of cars.children){
+        var option = document.createElement("option");
+        option.setAttribute('value',i);
+        i++;
+        option.text = car.children[0].textContent;
+        select1.add(option);
     }
 }
 
 function addCar() {
     validateCar();
-    if(isLoaded && isCarValid){
-        var newCar = fileContent.createElement("samochod");
+    if(isCarValid){
+        var samochod = fileContent.createElement("samochod");
         var newModel = fileContent.createElement("model");
         var newSegment = fileContent.createElement("segment");
-        var newPojemnosc = fileContent.createElement("pojemnosc_silnika");
-        var newUklad = fileContent.createElement("uklad_cylindrow");
+        var newMoc = fileContent.createElement("moc_maksymalna");
         var newSpalanie = fileContent.createElement("srednie_spalanie");
         var newData = fileContent.createElement("pierwsza_rejestracja");
+        var newCena = fileContent.createElement("cena");
         newModel.textContent = model.value;
         newSegment.textContent = segment.value;
-        newPojemnosc.textContent = pojemnosc.value;
-        newPojemnosc.setAttribute("jednostka","cm^3");
-        newUklad.textContent = uklad.value;
+        newMoc.textContent = moc.value;
+        newMoc.setAttribute("jednostka","KM");
         newSpalanie.textContent = spalanie.value;
         newSpalanie.setAttribute("jednostka","l/100km");
-        newData.textContent = data.value;
-        newCar.appendChild(newModel);
-        newCar.appendChild(newSegment);
-        newCar.appendChild(newPojemnosc);
-        newCar.appendChild(newUklad);
-        newCar.appendChild(newSpalanie);
-        newCar.appendChild(newData);
-        newCar.setAttribute("pozycja", pozycja.value);
-        newCar.setAttribute("seriaID", seriaID.value);
-        fileContent.querySelector("samochody").appendChild(newCar);
+        newData.textContent = data.value.replace("-",".");
+        newCena.textContent = cena.value;
+        samochod.appendChild(newModel);
+        samochod.appendChild(newSegment);
+        samochod.appendChild(newMoc);
+        samochod.appendChild(newSpalanie);
+        samochod.appendChild(newData);
+        samochod.appendChild(newCena);
+        samochod.setAttribute("seriaID", seria.value);
+        samochod.setAttribute("pozycja", "P"+pozycja+1);
+        fileContent.querySelector("samochody").appendChild(samochod);
     }
 }
-
 
 function deleteCar() {
     var id = document.getElementById('deleteCar').value;
@@ -123,36 +137,21 @@ function deleteCar() {
 }
 
 
-function removeOptions(){
-    const seriaSelect = document.getElementById('seria');
-    while (seriaSelect.length > 0) {
-        seriaSelect.remove(0);
-    }
-    const deleteCarSelect = document.getElementById('deleteCar');
-    while (deleteCarSelect.length > 0) {
-        deleteCarSelect.remove(0);
-    }
-    const modifyCarSelect = document.getElementById('modifyCar');
-    while (modifyCarSelect.length > 0) {
-        modifyCarSelect.remove(0);
-    }
-}
-
 function modifyCar() {
     validateModifyCar();
-    if(isLoaded && isModifyCarValid){
+    if(isModifyCarValid){
         var id = document.getElementById('pickCar').value;
         var car = fileContent.querySelector('samochody').children[id-1];
-        var newModel = document.getElementById('newModel');
-        var newSegment = document.getElementById('newSegment');
-        var newPojemnosc = document.getElementById('newPojemnosc');
-        var newUklad = document.getElementById('newUklad');
-        var newSpalanie = document.getElementById('newSpalanie');
-        var newData = document.getElementById('newData');
-        var features = [newModel,newSegment,newPojemnosc,newUklad,newSpalanie,newData];
+        var newModel = document.getElementById('nowyModel');
+        var newSegment = document.getElementById('nowySegment');
+        var newMoc = document.getElementById('nowaMoc');
+        var newSpalanie = document.getElementById('noweSpalanie');
+        var newData = document.getElementById('nowaData');
+        var newCena = document.getElementById('nowyCena');
+        var features = [newModel,newSegment,newMoc,newSpalanie,newData,newCena];
         var i=0;
         for(const feature of car.children){
-            if(features[i].value != '00' && features[i].value !=''){
+            if(features[i].value !=''){
                 feature.textContent = features[i].value;
             }
             i++;
@@ -160,86 +159,127 @@ function modifyCar() {
     }
 }
 
-
+// git w chuj
 function validateCar() {
     isCarValid = true;
 
-    // Clear the warning messages
-    warning1.innerHTML = "";
-    warning2.innerHTML = "";
-    warning3.innerHTML = "";
-    warning4.innerHTML = "";
-
     // Validate the input fields
-    if (seria.value == "") {
-        warning1.innerHTML = "Please select a series";
-        isCarValid = false;
+    if(!(/^[A-Z]{1}[0-9]{2}$/).test(model.value) || model.value ==""){
+        warning1.textContent ="model doesnt match [A-Z]{1}[0-9]{2} ";
+        warning1.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    if (segment.value == "") {
-        warning2.innerHTML = "Please select a segment";
-        isCarValid = false;
+    else{
+        warning1.textContent ="";
+        warning1.style.cssText="";
     }
-    if (model.value == "") {
-        warning3.innerHTML = "Please enter a model name";
-        isCarValid = false;
+
+    if(!(/^[A-Z]{1}$/).test(segment.value) || segment.value==''){
+        warning2.textContent ="segment doesnt match [A-Z]{1}[a-z]{0,14} ";
+        warning2.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    if (pojemnosc.value == "" || isNaN(pojemnosc.value)) {
-        warning4.innerHTML = "Please enter a valid capacity";
-        isCarValid = false;
+    else{
+        warning2.textContent ="";
+        warning2.style.cssText="";
     }
-    if (uklad.value == "") {
-        warning5.innerHTML = "Please enter a layout";
-        isCarValid = false;
+
+    if(!(/^[0-9]{1,3}$/).test(moc.value) || moc.value==''){
+        warning3.textContent ="moc doesnt match [0-9]{1,3} ";
+        warning3.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    if (spalanie.value == "" || isNaN(spalanie)) {
-        warning6.innerHTML = "Please enter a valid fuel consumption";
-        isCarValid = false;
+    else{
+        warning3.textContent ="";
+        warning3.style.cssText="";
     }
-    if (data.value == "") {
-        warning7.innerHTML = "Please select a registration date";
-        isCarValid = false;
+
+    if(!(/^[0-9]{1,2}$/).test(spalanie.value) || spalanie.value==''){
+        warning4.textContent ="spalanie doesnt match [0-9]{1,2} ";
+        warning4.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    return isCarValid;
+    else{
+        warning4.textContent ="";
+        warning4.style.cssText="";
+    }
+
+    if(!(/^[0-9]{1,3}$/).test(cena.value) || cena.value==''){
+        warning5.textContent ="cena doesnt match [0-9]{0,8} ";
+        warning5.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
+    }
+    else{
+        warning5.textContent ="";
+        warning5.style.cssText="";
+    }
 }
 
+//git w chuj
 function validateModifyCar() {
     isModifyCarValid = true;
 
-    var nowaSeria = document.getElementById('nowaSeria');
+    var nowyModel = document.getElementById('nowyModel');
     var nowySegment = document.getElementById('nowySegment');
+    var nowaMoc = document.getElementById('nowaMoc');
     var noweSpalanie = document.getElementById('noweSpalanie');
-    var nowaPojemnosc = document.getElementById('nowaPojemnosc');
+    var nowyCena = document.getElementById('nowyCena');
 
-    // Clear the warning messages
-    warning5.innerHTML = "";
-    warning6.innerHTML = "";
-    warning7.innerHTML = "";
-    warning8.innerHTML = "";
+    if(!(/^[A-Z]{1}[0-9]{2}$/).test(nowyModel.value) || nowyModel.value ==""){
+        warning6.textContent ="model doesnt match [A-Z]{1}[0-9]{2} ";
+        warning6.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
+    }
+    else{
+        warning6.textContent ="";
+        warning6.style.cssText="";
+    }
 
-    // Validate the input fields
-    if (nowaSeria.value === "") {
-        warning5.innerHTML = "Please select a series";
-        isModifyCarValid = false;
+    if(!(/^[A-Z]{1}$/).test(nowySegment.value) || nowySegment.value==''){
+        warning7.textContent ="segment doesnt match [A-Z]{1}[a-z]{0,14} ";
+        warning7.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    if (nowySegment.value === "") {
-        warning6.innerHTML = "Please select a segment";
-        isModifyCarValid = false;
+    else{
+        warning7.textContent ="";
+        warning7.style.cssText="";
     }
-    if (noweSpalanie.value === "") {
-        warning7.innerHTML = "Please enter a model name";
-        isModifyCarValid = false;
+
+    if(!(/^[0-9]{1,3}$/).test(nowaMoc.value) || nowaMoc.value==''){
+        warning8.textContent ="moc doesnt match [0-9]{1,3} ";
+        warning8.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
     }
-    if (nowaPojemnosc.value === "" || isNaN(nowaPojemnosc.value)) {
-        warning8.innerHTML = "Please enter a valid capacity";
-        isModifyCarValid = false;
+    else{
+        warning8.textContent ="";
+        warning8.style.cssText="";
     }
-    return isModifyCarValid;
+
+    if(!(/^[0-9]{1,2}$/).test(noweSpalanie.value) || noweSpalanie.value==''){
+        warning9.textContent ="spalanie doesnt match [0-9]{1,2} ";
+        warning9.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
+    }
+    else{
+        warning9.textContent ="";
+        warning9.style.cssText="";
+    }
+
+    if(!(/^[0-9]{1,8}$/).test(nowyCena.value) || nowyCena.value==''){
+        warning10.textContent ="cena doesnt match [0-9]{0,8}'.00' ";
+        warning10.style.cssText=  "display: block;width: 100%;background-color: #FF2734;text-align: center;padding-top: 10px;padding-bottom: 10px;margin-bottom: 20px;border-radius: 10px;box-shadow: 2px 2px #000;";
+        isCarValid= false;
+    }
+    else{
+        warning10.textContent ="";
+        warning10.style.cssText="";
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     loadButton.addEventListener("click", loadFile);
     saveButton.addEventListener("click", handleSaveButton);
-  addButton.addEventListener('click',addCar);
+    addButton.addEventListener('click',addCar);
     addButton.addEventListener('click',addDeleteOptions);
     deleteCarButton.addEventListener('click',deleteCar);
     modifyCarButton.addEventListener('click',modifyCar);
