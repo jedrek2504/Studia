2cf6cfc039c9392f719fc5b5a07d3cf0da0225ee
242430 Kostyk Jędrzej <242430@edu.p.lodz.pl>
2023-01-14 17:27:28 +0100

    zad6 finished

--- Web/zad6/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll(".button-nav");
-
-buttons.forEach((button, i) =>
-    button.addEventListener("click", function () {
-        if (!i) document.querySelector(".tasks-grid").scrollIntoView({ behavior: "smooth" });
-        else document.querySelector(`.box${i}`).scrollIntoView({ behavior: "smooth" });
-    })
-);
